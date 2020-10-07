42fd5275286d041353679ae03dafcc13e6f15c1f
Aidas Bulakas <aidas.bulakas@mif.stud.vu.lt>
2020-10-07 22:43:56 +0300

    Basic program start created

---
+++ HashFunction/HashFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HashFunction
+{
+    class Program
+    {
+        public static string inputString;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string:");
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Hash function:\n" + hashFunc(inputString));
+        }
+        static string hashFunc(string inputString)
+        {
+            string hashString;
+
+            // TODO: Hash logic
+
+            hashString = "";
+            return hashString;
+        }
+    }
+}
